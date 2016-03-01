6518db28281b9b33b2c729268664e56c892f5bbd
Sandra Adams-Hallie <Sandra@sandras-macbook-pro.local>
2016-03-01 11:09:40 +0100

    Initial Commit

---
+++ Noughts and Crosses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noughts and Crosses
+//
+//  Created by Sandra Adams-Hallie on Mar/1/16.
+//  Copyright © 2016 Sandra Adams-Hallie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
